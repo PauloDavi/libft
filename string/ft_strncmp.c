60f3452e66cc9d2891e0882af4bb61acdce58fd8
luizedua42 <luizedua@student.42sp.org.br>
2023-10-22 22:52:06 -0300

    improve strncmp

--- string/ft_strncmp.c
+++ string/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pdavi-al <pdavi-al@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 19:05:11 by pdavi-al          #+#    #+#             */
-/*   Updated: 2023/05/23 20:38:42 by pdavi-al         ###   ########.fr       */
+/*   Updated: 2023/10/22 22:46:47 by luizedua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
 	while ((*s1 + *s2) != '\0' && n--)
 	{
 		if (*s1 != *s2)
